80ae88ab8c2ebfdfdc80ec0b8154d6cbb815fe12
jlauza <lauzajayson@gmail.com>
2023-06-26 00:33:14 +0800

    added comment for explaination

--- server/db/conn.mjs
+++ server/db/conn.mjs
@@ -1,17 +1,21 @@
+// First import MongoClient from mongoDB
 import { MongoClient } from "mongodb";
 
+// Pull db uri from .env file
 const connectionString = process.env._URI || "";
 
+// assign the connectionString variable to MongoClient class
 const client = new MongoClient(connectionString);
 
+// Test connection with try-catch
 let conn;
 try {
   conn = await client.connect();
-  console.log("Successful connection with MongoDB!");
+  console.log("Successfully connected to MongoDB!");
 } catch(e) {
   console.error(e);
 }
 
-let db = conn.db("sample_training");
+let db = conn.db("test");
 
 export default db;
