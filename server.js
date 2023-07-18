cbde4db86263d6f7b03547b0914381b9c8ee68a3
jlauza <lauzajayson@gmail.com>
2023-07-18 23:06:49 +0800

    added public folder and store all views

--- server.js
+++ server.js
@@ -6,26 +6,17 @@ const port = 3000;
 
 // Entry point for the app
 app.get("/", (req, res) => {
-  // res.send("Please login");
-  const filePath = path.join(__dirname, "index.html");
+  // const filePath = path.join(__dirname, "index.html");
+  const filePath = path.join(__dirname, "public", "home", "index.html");
   res.sendFile(filePath);
 });
 
-// Route for the admin section
-// Serve static files
-app.use(express.static(path.join(__dirname, "admin", "public")));
-
-// Route handler for serving the React app
-app.get("/admin", (req, res) => {
-  res.sendFile(path.join(__dirname, "admin", "public", "index.html"));
+app.get("/auth", (req, res) => {
+  const filePathHome = path.join(__dirname, "public", "auth", "index.html");
+  res.sendFile(filePathHome);
 });
 
-// Route for the user section
-// app.use("/user", require("./student/src/index")); // enable when user section is ready
-
-// Other routes here and middleware
-
 // Start server
-app.listen(3000, () => {
-  console.log("Server started on port 3000");
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
 });
